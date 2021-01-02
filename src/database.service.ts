4fcaff91f51d0e4aaee6b9cdcb62fafa05c7a323
Kerren Ortlepp <kerren@entrostat.com>
2021-01-02 21:35:57 +0200

    Create the database in the /tmp directory

--- src/database.service.ts
+++ src/database.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs-extra';
 import * as sqlite from 'sqlite3';
 import { open, Database } from 'sqlite';
 import { v4 as uuid } from 'uuid';
@@ -12,7 +14,17 @@ export class DatabaseService implements OnModuleInit {
 
   async onModuleInit() {
     try {
-      const db = await this.createDatabase();
+      const dbPath = path.resolve(
+        process.env.DB_STORAGE_PATH || '/tmp/database.sqlite',
+      );
+      this.logger.info(
+        `Database ${
+          fs.existsSync(dbPath) ? 'EXISTS' : 'DOES NOT EXIST'
+        } at ${dbPath}`,
+      );
+      const db = fs.existsSync(dbPath)
+        ? await this.openDatabase(dbPath)
+        : await this.createDatabase(dbPath);
       this.database = await db;
       this.logger.info(
         `The database has been opened successfully`,
@@ -23,16 +35,17 @@ export class DatabaseService implements OnModuleInit {
     }
   }
 
-  private async openDatabase() {
+  private async openDatabase(dbPath: string) {
     return open({
-      filename: ':memory:',
+      filename: dbPath,
       driver: sqlite.Database,
     });
   }
 
-  private async createDatabase() {
-    this.logger.info(`Create a new database at :memory:`);
-    await this.openDatabase().then(async (db) => {
+  private async createDatabase(dbPath: string) {
+    this.logger.info(`Create a new database at ${dbPath}`);
+    await fs.writeFile(dbPath, '');
+    await this.openDatabase(dbPath).then(async (db) => {
       await db.exec(
         `CREATE TABLE key_values (key TEXT PRIMARY KEY, value TEXT)`,
       );
@@ -41,7 +54,7 @@ export class DatabaseService implements OnModuleInit {
       await db.exec(`INSERT INTO tokens VALUES ('${initToken}')`);
       this.logger.info(`Created the initial token\n\n\n${initToken}\n\n`);
     });
-    return this.openDatabase();
+    return this.openDatabase(dbPath);
   }
 
   getDatabaseInstance() {
